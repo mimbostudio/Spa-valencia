ecc29e67ff696182783dd74374cdbd5348ab80c7
mimboagency <37906953+mimboagency@users.noreply.github.com>
2018-04-01 11:24:41 +0300

    Исправили инстаграмм

--- instagram.php
+++ instagram.php
@@ -1,15 +1,8 @@
 <?
 if($_GET["max"])
-$response = file_get_contents('https://www.instagram.com/valencia.spahotel/?__a=1&count=15&max_id='.$_GET["max"]);
+$response = file_get_contents('https://api.instagram.com/v1/users/3920794552/media/recent/?access_token=3920794552.1677ed0.79b294b858c14ce6b798e98fb45006b6&count=15&max_id='.$_GET["max"]);
 else
-$response = file_get_contents('https://www.instagram.com/valencia.spahotel/?__a=1&count=15');	
+$response = file_get_contents('https://api.instagram.com/v1/users/3920794552/media/recent/?access_token=3920794552.1677ed0.79b294b858c14ce6b798e98fb45006b6&count=15');	
 echo json_encode(json_decode($response), JSON_UNESCAPED_UNICODE);
 
-/*
-if($_GET["max"])
-$response = file_get_contents('https://api.instagram.com/v1/users/3920794552/media/recent/?access_token=3920794552.1677ed0.7b704721025c4af6a328329d93298852&count=15&max_id='.$_GET["max"]);
-else
-$response = file_get_contents('https://api.instagram.com/v1/users/3920794552/media/recent/?access_token=3920794552.1677ed0.7b704721025c4af6a328329d93298852&count=15');	
-echo json_encode(json_decode($response), JSON_UNESCAPED_UNICODE);
-/*
 ?>
